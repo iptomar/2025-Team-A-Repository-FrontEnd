c8ce82187c78e8189bce5d075503eb5e3cfeb75a
Aluno24845 <aluno24845@ipt.pt>
2025-05-04 00:32:52 +0100

    Adicionada funcionalidades de gestão de utilizadores:  Listagem e Detalhes

---
+++ src/components/Listas/ListaUtilizadores.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import * as Api from "../../api/api";
+import Lista from "../common/Lista";
+import Item from "../common/Item";
+
+export default function ListaUtilizadores() {
+    return (
+        <Lista
+            endpoint={Api.getUtilizadores}
+            colunas={["Nome", "Email", "Email Confirmado"]}
+            nomeEntidade="Utilizadores"
+            deleteFn={Api.eliminaUtilizador}
+            renderItem={(utilizador, handleDelete) => (
+                <Item
+                    key={utilizador.id}
+                    item={utilizador}
+                    campos={[
+                        (e) => e.nome,
+                        (e) => e.email,
+                        (e) => e.emailConfirmed ? "✅" : "❌",
+                    ]}
+                    detalhes="utilizadores/detalhes"
+                    editar="utilizadores/editar"
+                    deleteFn={handleDelete}
+                />
+            )}
+        />
+    );
+}
